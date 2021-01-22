ef287aac704176fe624b669bdb9948bb21102933
STEN1 <STEN1@users.noreply.github.com>
2021-01-22 22:25:44 +0100

    Initial commit

---
+++ Source/ProductionTopDown/ProductionTopDownGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProductionTopDownGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRODUCTIONTOPDOWN_API AProductionTopDownGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
