6435f326797e6d7c4c286b2836222c482513beae
STEN1 <STEN1@users.noreply.github.com>
2021-02-24 17:36:21 +0100

    Inventory can no store items.

--- Source/ProductionTopDown/Actors/Items/Weapons/RustySword.cpp
+++ Source/ProductionTopDown/Actors/Items/Weapons/RustySword.cpp
@@ -6,6 +6,8 @@
 ARustySword::ARustySword()
 {
 	ItemName = TEXT("Rusty Sword");
+	ItemLore = TEXT("A rusty piece of steel, forged by a blacksmiths apprentice.");
+	ThisItemClass = RustySword;
 }
 
 void ARustySword::BeginPlay()
--- Source/ProductionTopDown/Components/InventoryComponent.h
+++ Source/ProductionTopDown/Components/InventoryComponent.h
@@ -4,11 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "ProductionTopDown/Actors/Items/Weapons/WeaponBase.h"
 #include "InventoryComponent.generated.h"
 
 
 class AItemBase;
 class AWeaponBase;
+class AProductionTopDownGameModeBase;
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class PRODUCTIONTOPDOWN_API UInventoryComponent : public UActorComponent
 {
@@ -16,22 +19,45 @@ class PRODUCTIONTOPDOWN_API UInventoryComponent : public UActorComponent
 
 public:	
 	UInventoryComponent();
+	
+	struct FInventoryItem
+	{
+		ItemClass _ItemClass{Empty};
+		FString _ItemName{""};
+		FString _ItemLore{""};
+		float _MinDamage{ 0.f };
+		float _MaxDamage{ 0.f };
+		float _CritChance{ 0.f };
+	};
 
-
+	
 protected:
 	virtual void BeginPlay() override;
 
 private:
+
+
+	
 	UFUNCTION()
 	void BeginOverlap(AActor* OverlappedActor, AActor* OtherActor);
 	UFUNCTION()
 	void EndOverlap(AActor* OverlappedActor, AActor* OtherActor);
 	void Interact();
+	void Slot1();
+	void Slot2();
+	void Slot3();
+	void Slot4();
+	int32 CurrentSlot{ 1 };
 
-	void UpdateOverlapArrays();
-	TArray<AWeaponBase*> OverlappingWeapons;
+	void UpdateOverlapArray();
 	TArray<AItemBase*> OverlappingItems;
+	
+	TArray<FInventoryItem> Inventory;
 
 	UPROPERTY()
 	UInputComponent* PlayerInputComponent;
+	UPROPERTY()
+	AWeaponBase* EquippedWeapon;
+	UPROPERTY()
+	AProductionTopDownGameModeBase* GameModeRef;
 };
