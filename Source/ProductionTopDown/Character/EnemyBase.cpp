9feec4e503d81dcc877d0d9f37b9ec52dafd9d44
Eddy <Edvardh@hotmail.no>
2021-04-11 17:59:59 +0200

    Enemy Fixes

--- Source/ProductionTopDown/Character/EnemyBase.cpp
+++ Source/ProductionTopDown/Character/EnemyBase.cpp
@@ -52,6 +52,20 @@ void AEnemyBase::BeginPlay()
 	{
 		EnemyState = EEnemyState::Idle;
 	}
+
+	TArray<AActor*> OverlappingActors;
+	DetectionComponent->GetOverlappingActors(OverlappingActors);
+	if (OverlappingActors.Num() > 0)
+	{
+		for (int i = 0; i < OverlappingActors.Num(); ++i)
+		{
+			if (OverlappingActors[i]->IsA(APlayerCharacter::StaticClass()))
+			{
+				bIsPlayerClose = true;
+				i = OverlappingActors.Num();
+			}
+		}
+	}
 	
 }
 
@@ -248,7 +262,7 @@ FHitResult AEnemyBase::GetFirstHitInReach(ECollisionChannel CollisionChannel, FV
 void AEnemyBase::OnComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
     UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (OtherActor->IsA(ACharacterBase::StaticClass()) && OtherComp->IsA(UCapsuleComponent::StaticClass()))
+	if (OtherActor->IsA(APlayerCharacter::StaticClass()) && OtherComp->IsA(UCapsuleComponent::StaticClass()))
 	{
 		bIsPlayerClose = true;
 	}
