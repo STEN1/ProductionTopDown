3b9970674fd1b9728d27e972e76d8f8405168755
Adrian Drevland <adrian.l.drevland@gmail.com>
2021-04-25 19:12:14 +0200

    more boss work

--- Source/ProductionTopDown/Character/BossAiController.cpp
+++ Source/ProductionTopDown/Character/BossAiController.cpp
@@ -29,13 +29,22 @@ void ABossAiController::BeginPlay()
 	
 	BossBlackBoard = GetBlackboardComponent();
 	if(PlayerPawn && BossBlackBoard) BossBlackBoard->SetValueAsObject(TEXT("Player"), PlayerPawn->GetOwner());
+	if(BossBlackBoard) BossBlackBoard->SetValueAsVector(TEXT("RoomCenter"), GetOwner()->GetActorLocation()); // Location to use spin attack
 }
 
 void ABossAiController::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
 
-	if(PlayerPawn && BossBlackBoard)BossBlackBoard->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
-
+	if(BossBlackBoard)
+	{
+		if(PlayerPawn)BossBlackBoard->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+		
+		if(PlayerPawn && LineOfSightTo(PlayerPawn)) BossBlackBoard->SetValueAsBool(TEXT("HasLineOfSight"), true);
+		else BossBlackBoard->SetValueAsBool(TEXT("HasLineOfSight"), false);
+		
+		if(GetOwner()->GetDistanceTo(PlayerPawn) < 200) BossBlackBoard->SetValueAsBool(TEXT("InAttackRange"), true);
+		else BossBlackBoard->SetValueAsBool(TEXT("InAttackRange"), false);
+	}
 	
 }
