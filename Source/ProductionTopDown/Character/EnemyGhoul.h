4af6d6f7abcb04057961858228f4b7729b142fcc
Eddy <Edvardh@hotmail.no>
2021-04-26 20:19:13 +0200

    Added Temp assets for Ghoul and Adventurer

---
+++ Source/ProductionTopDown/Character/EnemyGhoul.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyBase.h"
+#include "EnemyGhoul.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRODUCTIONTOPDOWN_API AEnemyGhoul : public AEnemyBase
+{
+	GENERATED_BODY()
+	
+};
